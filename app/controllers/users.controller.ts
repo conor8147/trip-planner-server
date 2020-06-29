c98c39b86bc5aa7996ad8c2347a409964281bb79
Conor O'Neill <conor8147@gmail.com>
2020-06-30 10:53:43 +1200

    Added isEmailValid and some commenting

--- app/controllers/users.controller.ts
+++ app/controllers/users.controller.ts
@@ -2,6 +2,19 @@ import express = require('express');
 
 const users = require('../models/users.model.ts');
 
+/**
+ * Checks only that a candidate email address contains the @ symbol.
+ * @param email a string email address
+ */
+function isEmailValid(email: string) {
+    return email.includes('@');
+}
+
+/**
+ * Receive a user request to login. If the
+ * @param req an express request. Body should contain an email address and hashed password.
+ * @param res an express response with status 200 if login succeeds, 400 if failed.
+ */
 exports.login = async function (req: express.Request, res: express.Response) {
     // Handle login here
 };
